1b9cff56c7740e0f87658d7a4a84218924464b3e
Ntobeko Sikithi <ntobekosikithi@woolworths.co.za>
2025-02-20 15:52:11 +0200

    Initial Commit

---
+++ CoinRank/CoinRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRankApp.swift
+//  CoinRank
+//
+//  Created by Ntobeko Sikithi on 2025/02/20.
+//
+
+import SwiftUI
+
+@main
+struct CoinRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinRank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinRank
+//
+//  Created by Ntobeko Sikithi on 2025/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
